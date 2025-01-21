4413d037b5d3ea42e517f2e5f3c65953cc6a178b
Hasnainmustafa110 <writeshasnain@gmail.com>
2025-01-21 17:00:19 +0500

    Guessgame.cpp

--- Guessgame.cpp
+++ Guessgame.cpp
@@ -2,37 +2,47 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+
 int main() {
-    // Seed the random number generator
-    srand(static_cast<unsigned>(time(0)));
+    char playAgain;
+    
+    do {
+        // Seed the random number generator
+        srand(static_cast<unsigned>(time(0)));
 
-    // Generate a random number between 1 and 100
-    int numberToGuess = rand() % 100 + 1;
-    int userGuess = 0; // Variable to store the user's guess
-    int attempts = 0;  // Counter for attempts
-   
-   
-    cout<<"\t\t\t*************************X************************************"<<endl;
+        // Generate a random number between 1 and 100
+        int numberToGuess = rand() % 100 + 1;
+        int userGuess = 0; // Variable to store the user's guess
+        int attempts = 0;  // Counter for attempts
 
-    cout << "\t\t\t\tWelcome to the Guess the Number Game!" << endl;
-    cout<<"\t\t\t*************************X************************************"<<endl;
-    cout << "I have selected a number between 1 and 100. Can you guess it?" << endl;
+        cout << "\t\t\t*************************X************************************" << endl;
+        cout << "\t\t\t\tWelcome to the Guess the Number Game!" << endl;
+        cout << "\t\t\t*************************X************************************" << endl;
+        cout << "I have selected a number between 1 and 100. Can you guess it?" << endl;
 
-    // Game loop
-    while (userGuess != numberToGuess) {
-        cout << "Enter your guess: ";
-        cin >> userGuess;
+        // Game loop
+        while (userGuess != numberToGuess) {
+            cout << "Enter your guess: ";
+            cin >> userGuess;
 
-        if (userGuess < numberToGuess) {
-            cout << "Too low! Try again." << endl;
-        } else if (userGuess > numberToGuess) {
-            cout << "Too high! Try again." << endl;
-        } else {
-            cout << "Congratulations! You guessed the number!" << endl;
+            if (userGuess < numberToGuess) {
+                cout << "Too low! Try again." << endl;
+            } else if (userGuess > numberToGuess) {
+                cout << "Too high! Try again." << endl;
+            } else {
+                cout << "Congratulations! You guessed the number!" << endl;
+            }
+            attempts++;
         }
-        attempts++;
-    }
 
-    cout << "It took you " << attempts << " attempts to guess the number." << endl;
+        cout << "It took you " << attempts << " attempts to guess the number." << endl;
+
+        // Ask if the user wants to play again
+        cout << "Do you want to play again? (y/n): ";
+        cin >> playAgain;
+
+    } while (playAgain == 'y' || playAgain == 'Y');
+
+    cout << "Thank you for playing! Goodbye!" << endl;
     return 0;
 }
